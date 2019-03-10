4617d0a84185f54586041dbdc4dc3ec050cd5ee8
kevin2431 <164058292@qq.com>
2019-03-10 21:09:43 +0800

    test for genome desktop

--- setup_host/install-nvidia-drivers.sh
+++ setup_host/install-nvidia-drivers.sh
@@ -1,3 +1,13 @@
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.25/NVIDIA-Linux-x86_64-390.25.run
-chmod a+x NVIDIA-Linux-x86_64-390.25.run
-./NVIDIA-Linux-x86_64-390.25.run -s --install-libglvnd
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-9-0; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  apt-get update
+  apt-get install cuda-9-0 -y
+fi
+# Enable persistence mode
+nvidia-smi -pm 1
